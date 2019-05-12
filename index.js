c3b23ae67ca17f9c574ef871002604d51bd3f370
Doug Loomer <loomerds@gmail.com>
2019-05-12 18:10:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function johnLennonFacts(facts) {
   let arr = [];
   let i = 0;
   while(i<facts.length) {
-    arr.push(`${facts[i]} !!!`);
+    arr.push(`${facts[i]}!!!`);
     i++;
   }
   return arr;
